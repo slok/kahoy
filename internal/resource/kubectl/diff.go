869d92ab445bc6c49897212969dc814f93e25a10
Xabier Larrakoetxea <me@slok.dev>
2020-08-13 07:12:00 +0200

    Implement diff resource manager based on kubectl and diff external commands
    Signed-off-by: Xabier Larrakoetxea <me@slok.dev>

---
+++ internal/resource/kubectl/diff.go
@@ -0,0 +1,257 @@
+package kubectl
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+
+	"github.com/slok/kahoy/internal/log"
+	"github.com/slok/kahoy/internal/model"
+	"github.com/slok/kahoy/internal/resource"
+)
+
+// CmdRunner knows how to run exec.Cmd commands.
+type CmdRunner interface {
+	Run(*exec.Cmd) error
+}
+
+//go:generate mockery --case underscore --output kubectlmock --outpkg kubectlmock --name CmdRunner
+
+type stdCmdRunner struct {
+	logger log.Logger
+}
+
+func (s stdCmdRunner) Run(c *exec.Cmd) error {
+	s.logger.Debugf("running cmd: '%s'", c)
+	return c.Run()
+}
+
+// K8sObjectEncoder knows how to encode K8s objects into Raw Kubernetes compatible formats.
+type K8sObjectEncoder interface {
+	EncodeObjects(ctx context.Context, objs []model.K8sObject) ([]byte, error)
+}
+
+//go:generate mockery --case underscore --output kubectlmock --outpkg kubectlmock --name K8sObjectEncoder
+
+// FSManager knows how to manage resources on the fs.
+type FSManager interface {
+	TempDir(dir, pattern string) (name string, err error)
+	RemoveAll(path string) error
+	WriteFile(filename string, data []byte, perm os.FileMode) error
+}
+
+type stdFSManager struct{}
+
+//go:generate mockery --case underscore --output kubectlmock --outpkg kubectlmock --name FSManager
+
+func (stdFSManager) TempDir(dir, pattern string) (name string, err error) {
+	return ioutil.TempDir(dir, pattern)
+}
+
+func (stdFSManager) WriteFile(filename string, data []byte, perm os.FileMode) error {
+	return ioutil.WriteFile(filename, data, perm)
+}
+
+func (stdFSManager) RemoveAll(path string) error { return os.RemoveAll(path) }
+
+// DiffManagerConfig is the configuration for NewDiffManager.
+type DiffManagerConfig struct {
+	KubectlCmd                string
+	KubeConfig                string
+	KubeContext               string
+	KubeFieldManager          string
+	DisableKubeForceConflicts bool
+	YAMLEncoder               K8sObjectEncoder
+	FSManager                 FSManager
+	CmdRunner                 CmdRunner
+	Out                       io.Writer
+	ErrOut                    io.Writer
+	Logger                    log.Logger
+}
+
+func (c *DiffManagerConfig) defaults() error {
+	if c.KubectlCmd == "" {
+		c.KubectlCmd = "kubectl"
+	}
+
+	if c.Out == nil {
+		c.Out = os.Stdout
+	}
+
+	if c.ErrOut == nil {
+		c.ErrOut = os.Stderr
+	}
+
+	if c.Logger == nil {
+		c.Logger = log.Noop
+	}
+	c.Logger = c.Logger.WithValues(log.Kv{"app-svc": "kubectl.DiffManager"})
+
+	if c.CmdRunner == nil {
+		c.CmdRunner = stdCmdRunner{
+			logger: c.Logger.WithValues(log.Kv{"app-svc": "kubectl.StdCmdRunner"}),
+		}
+	}
+
+	if c.YAMLEncoder == nil {
+		return fmt.Errorf("yaml encoder is required")
+	}
+
+	if c.FSManager == nil {
+		c.FSManager = stdFSManager{}
+	}
+
+	return nil
+}
+
+type diffManager struct {
+	kubectlCmd  string
+	yamlEncoder K8sObjectEncoder
+	cmdRunner   CmdRunner
+	fsManager   FSManager
+	out         io.Writer
+	errOut      io.Writer
+	logger      log.Logger
+
+	applyArgs []string
+}
+
+// NewDiffManager returns a resource Manager based on Kubctl that will
+// output diff changes.
+func NewDiffManager(config DiffManagerConfig) (resource.Manager, error) {
+	err := config.defaults()
+	if err != nil {
+		return nil, fmt.Errorf("invalid configuration: %w", err)
+	}
+
+	applyArgs := newKubectlCmdArgs([]kubectlCmdOption{
+		withDiffCmd(),
+		withContext(config.KubeContext),
+		withConfig(config.KubeConfig),
+		withForceConflicts(!config.DisableKubeForceConflicts),
+		withFieldManager(config.KubeFieldManager),
+		withServerSide(true),
+		withStdIn(),
+	})
+
+	return diffManager{
+		kubectlCmd:  config.KubectlCmd,
+		yamlEncoder: config.YAMLEncoder,
+		cmdRunner:   config.CmdRunner,
+		fsManager:   config.FSManager,
+		out:         config.Out,
+		errOut:      config.ErrOut,
+		logger:      config.Logger,
+		applyArgs:   applyArgs,
+	}, nil
+}
+
+func (d diffManager) Apply(ctx context.Context, resources []model.Resource) error {
+	if len(resources) == 0 {
+		return nil
+	}
+
+	objs := make([]model.K8sObject, 0, len(resources))
+	for _, r := range resources {
+		objs = append(objs, r.K8sObject)
+	}
+
+	// Encode objects.
+	yamlData, err := d.yamlEncoder.EncodeObjects(ctx, objs)
+	if err != nil {
+		return fmt.Errorf("could not encode objects to diff: %w", err)
+	}
+
+	// Create command.
+	in := bytes.NewReader(yamlData)
+	cmd := d.newKubctlCmd(ctx, d.applyArgs, in)
+
+	// Execute command.
+	err = d.cmdRunner.Run(cmd)
+	if err != nil {
+		exitErr, ok := err.(*exec.ExitError)
+		// No error if our error is 1 exit code, just changes on diff.
+		// Check: https://github.com/kubernetes/kubernetes/pull/87437
+		if ok && exitErr.ExitCode() < 2 {
+			return nil
+		}
+
+		return fmt.Errorf("error while running apply diff command: %w", err)
+	}
+
+	return nil
+}
+
+// TODO(slok): At this moment we are making client-side diff with the stored state instead the server one.
+// 			   Implement server-side diff, get the resource and check what needs to be removed.
+func (d diffManager) Delete(ctx context.Context, resources []model.Resource) error {
+	if len(resources) == 0 {
+		return nil
+	}
+
+	// Create a temporal directory.
+	dirPath, err := d.fsManager.TempDir("", "KAHOY-")
+	if err != nil {
+		return fmt.Errorf("could not create temp dir: %w", err)
+	}
+	defer func() {
+		err := d.fsManager.RemoveAll(dirPath)
+		if err != nil {
+			d.logger.Warningf("could not delete %q diff temp dir: %s", dirPath, err)
+		}
+	}()
+
+	// We are creating a diff for each resource.
+	for _, r := range resources {
+		// Encode object.
+		yamlData, err := d.yamlEncoder.EncodeObjects(ctx, []model.K8sObject{r.K8sObject})
+		if err != nil {
+			return fmt.Errorf("could not encode objects to diff: %w", err)
+		}
+
+		// Store our YAML in a file.
+		gvk := r.K8sObject.GetObjectKind().GroupVersionKind()
+		fileName := strings.Join([]string{gvk.Group, gvk.Version, gvk.Kind, r.K8sObject.GetNamespace(), r.K8sObject.GetName()}, ".")
+		filePath := path.Join(dirPath, fileName)
+		err = d.fsManager.WriteFile(filePath, yamlData, 0644)
+		if err != nil {
+			return fmt.Errorf("could not write in file: %w", err)
+		}
+
+		// Create a diff commmand with the 2nd file as empty and execute.
+		cmd := d.newDiffCmd(ctx, filePath, strings.NewReader(""))
+		err = d.cmdRunner.Run(cmd)
+		if err != nil {
+			exitErr, ok := err.(*exec.ExitError)
+			// No error if our error is 1 exit code, just changes on diff.
+			if ok && exitErr.ExitCode() >= 2 {
+				return fmt.Errorf("error while running delete diff command: %w", err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (d diffManager) newDiffCmd(ctx context.Context, fileA string, in io.Reader) *exec.Cmd {
+	args := []string{"-u", "-N", fileA, "-"}
+	cmd := exec.CommandContext(ctx, "diff", args...)
+	cmd.Stdin = in
+	cmd.Stdout = d.out
+	cmd.Stderr = d.errOut
+	return cmd
+}
+
+func (d diffManager) newKubctlCmd(ctx context.Context, args []string, in io.Reader) *exec.Cmd {
+	cmd := exec.CommandContext(ctx, d.kubectlCmd, args...)
+	cmd.Stdin = in
+	cmd.Stdout = d.out
+	cmd.Stderr = d.errOut
+	return cmd
+}
---
+++ internal/resource/kubectl/diff_test.go
@@ -0,0 +1,333 @@
+package kubectl_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	"github.com/slok/kahoy/internal/model"
+	"github.com/slok/kahoy/internal/resource/kubectl"
+	"github.com/slok/kahoy/internal/resource/kubectl/kubectlmock"
+)
+
+// expCmdMatcher returns an *exec.Cmd matcher for  mock.MatchedBy.
+func expCmdMatcher(expArgs []string, expInput string) func(cmd *exec.Cmd) bool {
+	return func(cmd *exec.Cmd) bool {
+		data, err := ioutil.ReadAll(cmd.Stdin)
+		if err != nil {
+			return false
+		}
+
+		if string(data) != expInput {
+			return false
+		}
+
+		if len(expArgs) != len(cmd.Args) {
+			return false
+		}
+
+		for i, arg := range expArgs {
+			if arg != cmd.Args[i] {
+				return false
+			}
+		}
+
+		return true
+	}
+}
+
+func newK8sObject(name, ns string) model.K8sObject {
+	return &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "apps/v1",
+			"kind":       "Deployment",
+			"metadata": map[string]interface{}{
+				"name":      name,
+				"namespace": ns,
+			},
+		},
+	}
+}
+
+func TestDiffManagerApply(t *testing.T) {
+	tests := map[string]struct {
+		config    kubectl.DiffManagerConfig
+		resources []model.Resource
+		mock      func(mk *kubectlmock.K8sObjectEncoder, mc *kubectlmock.CmdRunner)
+		expErr    bool
+	}{
+		"Not having resources, shouldn't execute anything.": {
+			resources: []model.Resource{},
+			mock:      func(mk *kubectlmock.K8sObjectEncoder, mc *kubectlmock.CmdRunner) {},
+		},
+
+		"Having an error while encoding objects should stop the execution and fail.": {
+			resources: []model.Resource{{Name: "test1"}},
+			mock: func(mk *kubectlmock.K8sObjectEncoder, mc *kubectlmock.CmdRunner) {
+				mk.On("EncodeObjects", mock.Anything, mock.Anything).Once().Return(nil, errors.New("whatever"))
+			},
+			expErr: true,
+		},
+
+		"Having an error while running the cmd should stop the execution and fail.": {
+			resources: []model.Resource{{Name: "test1"}},
+			mock: func(mk *kubectlmock.K8sObjectEncoder, mc *kubectlmock.CmdRunner) {
+				mk.On("EncodeObjects", mock.Anything, mock.Anything).Once().Return(nil, nil)
+				mc.On("Run", mock.Anything).Once().Return(errors.New("whatever"))
+			},
+			expErr: true,
+		},
+
+		"Having resources should apply correctly.": {
+			resources: []model.Resource{
+				{Name: "test1", K8sObject: newK8sObject("test1", "ns1")},
+				{Name: "test2", K8sObject: newK8sObject("test2", "ns1")},
+			},
+			mock: func(mk *kubectlmock.K8sObjectEncoder, mc *kubectlmock.CmdRunner) {
+				expK8sResources := []model.K8sObject{
+					newK8sObject("test1", "ns1"),
+					newK8sObject("test2", "ns1"),
+				}
+				mk.On("EncodeObjects", mock.Anything, expK8sResources).Once().Return([]byte("test"), nil)
+
+				exp := expCmdMatcher(
+					[]string{"kubectl", "diff", "--force-conflicts=true", "--server-side=true", "-f", "-"},
+					"test",
+				)
+				mc.On("Run", mock.MatchedBy(exp)).Once().Return(nil)
+			},
+		},
+
+		"Having custom command should apply with the specific cmd.": {
+			config: kubectl.DiffManagerConfig{
+				KubectlCmd: "whatever",
+			},
+			resources: []model.Resource{{Name: "test1"}},
+			mock: func(mk *kubectlmock.K8sObjectEncoder, mc *kubectlmock.CmdRunner) {
+				mk.On("EncodeObjects", mock.Anything, mock.Anything).Once().Return([]byte("test"), nil)
+
+				exp := expCmdMatcher(
+					[]string{"whatever", "diff", "--force-conflicts=true", "--server-side=true", "-f", "-"},
+					"test",
+				)
+				mc.On("Run", mock.MatchedBy(exp)).Once().Return(nil)
+			},
+		},
+
+		"Having kubectl context set, should set the cmd flag.": {
+			config: kubectl.DiffManagerConfig{
+				KubeContext: "whatever",
+			},
+			resources: []model.Resource{{Name: "test1"}},
+			mock: func(mk *kubectlmock.K8sObjectEncoder, mc *kubectlmock.CmdRunner) {
+				mk.On("EncodeObjects", mock.Anything, mock.Anything).Once().Return([]byte("test"), nil)
+
+				exp := expCmdMatcher(
+					[]string{"kubectl", "diff", "--context", "whatever", "--force-conflicts=true", "--server-side=true", "-f", "-"},
+					"test",
+				)
+				mc.On("Run", mock.MatchedBy(exp)).Once().Return(nil)
+			},
+		},
+
+		"Having kubectl config set, should set the cmd flag.": {
+			config: kubectl.DiffManagerConfig{
+				KubeConfig: "whatever",
+			},
+			resources: []model.Resource{{Name: "test1"}},
+			mock: func(mk *kubectlmock.K8sObjectEncoder, mc *kubectlmock.CmdRunner) {
+				mk.On("EncodeObjects", mock.Anything, mock.Anything).Once().Return([]byte("test"), nil)
+
+				exp := expCmdMatcher(
+					[]string{"kubectl", "diff", "--kubeconfig", "whatever", "--force-conflicts=true", "--server-side=true", "-f", "-"},
+					"test",
+				)
+				mc.On("Run", mock.MatchedBy(exp)).Once().Return(nil)
+			},
+		},
+
+		"Having force disabled, should set the cmd flag.": {
+			config: kubectl.DiffManagerConfig{
+				DisableKubeForceConflicts: true,
+			},
+			resources: []model.Resource{{Name: "test1"}},
+			mock: func(mk *kubectlmock.K8sObjectEncoder, mc *kubectlmock.CmdRunner) {
+				mk.On("EncodeObjects", mock.Anything, mock.Anything).Once().Return([]byte("test"), nil)
+
+				exp := expCmdMatcher(
+					[]string{"kubectl", "diff", "--force-conflicts=false", "--server-side=true", "-f", "-"},
+					"test",
+				)
+				mc.On("Run", mock.MatchedBy(exp)).Once().Return(nil)
+			},
+		},
+
+		"Having field manager set, should set the cmd flag.": {
+			config: kubectl.DiffManagerConfig{
+				KubeFieldManager: "whatever",
+			},
+			resources: []model.Resource{{Name: "test1"}},
+			mock: func(mk *kubectlmock.K8sObjectEncoder, mc *kubectlmock.CmdRunner) {
+				mk.On("EncodeObjects", mock.Anything, mock.Anything).Once().Return([]byte("test"), nil)
+
+				exp := expCmdMatcher(
+					[]string{"kubectl", "diff", "--force-conflicts=true", "--field-manager", "whatever", "--server-side=true", "-f", "-"},
+					"test",
+				)
+				mc.On("Run", mock.MatchedBy(exp)).Once().Return(nil)
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert := assert.New(t)
+			require := require.New(t)
+
+			// Mocks.
+			menc := &kubectlmock.K8sObjectEncoder{}
+			mcmd := &kubectlmock.CmdRunner{}
+			test.mock(menc, mcmd)
+
+			// Prepare.
+			test.config.Out = ioutil.Discard
+			test.config.YAMLEncoder = menc
+			test.config.CmdRunner = mcmd
+			manager, err := kubectl.NewDiffManager(test.config)
+			require.NoError(err)
+
+			// Execute.
+			err = manager.Apply(context.TODO(), test.resources)
+
+			// Check.
+			if test.expErr {
+				assert.Error(err)
+			} else if assert.NoError(err) {
+				menc.AssertExpectations(t)
+				mcmd.AssertExpectations(t)
+			}
+		})
+	}
+}
+
+func TestDiffManagerDelete(t *testing.T) {
+	tests := map[string]struct {
+		config    kubectl.DiffManagerConfig
+		resources []model.Resource
+		mock      func(mk *kubectlmock.K8sObjectEncoder, mc *kubectlmock.CmdRunner, mfs *kubectlmock.FSManager)
+		expErr    bool
+	}{
+		"Not having resources, shouldn't execute anything.": {
+			resources: []model.Resource{},
+			mock:      func(mk *kubectlmock.K8sObjectEncoder, mc *kubectlmock.CmdRunner, mfs *kubectlmock.FSManager) {},
+		},
+
+		"Having an error while creating the tmp dir should stop execution and fail.": {
+			resources: []model.Resource{
+				{Name: "test1", K8sObject: newK8sObject("test1", "ns1")},
+			},
+			mock: func(mk *kubectlmock.K8sObjectEncoder, mc *kubectlmock.CmdRunner, mfs *kubectlmock.FSManager) {
+				mfs.On("TempDir", mock.Anything, mock.Anything).Once().Return("", fmt.Errorf("whatever"))
+			},
+			expErr: true,
+		},
+
+		"Having an error while encoding resource should stop execution and fail.": {
+			resources: []model.Resource{
+				{Name: "test1", K8sObject: newK8sObject("test1", "ns1")},
+			},
+			mock: func(mk *kubectlmock.K8sObjectEncoder, mc *kubectlmock.CmdRunner, mfs *kubectlmock.FSManager) {
+				mfs.On("TempDir", mock.Anything, mock.Anything).Once().Return("", nil)
+				mfs.On("RemoveAll", mock.Anything).Once().Return(nil)
+
+				mk.On("EncodeObjects", mock.Anything, mock.Anything).Once().Return(nil, fmt.Errorf("whatever"))
+			},
+			expErr: true,
+		},
+
+		"Having an error while storing encoded resource should stop execution and fail.": {
+			resources: []model.Resource{
+				{Name: "test1", K8sObject: newK8sObject("test1", "ns1")},
+			},
+			mock: func(mk *kubectlmock.K8sObjectEncoder, mc *kubectlmock.CmdRunner, mfs *kubectlmock.FSManager) {
+				mfs.On("TempDir", mock.Anything, mock.Anything).Once().Return("", nil)
+				mfs.On("RemoveAll", mock.Anything).Once().Return(nil)
+
+				mk.On("EncodeObjects", mock.Anything, mock.Anything).Once().Return([]byte("test1"), nil)
+				mfs.On("WriteFile", mock.Anything, mock.Anything, mock.Anything).Once().Return(fmt.Errorf("whatever"))
+			},
+			expErr: true,
+		},
+
+		"Having resources should apply correctly.": {
+			resources: []model.Resource{
+				{Name: "test1", K8sObject: newK8sObject("test1", "ns1")},
+				{Name: "test2", K8sObject: newK8sObject("test2", "ns2")},
+			},
+			mock: func(mk *kubectlmock.K8sObjectEncoder, mc *kubectlmock.CmdRunner, mfs *kubectlmock.FSManager) {
+				mfs.On("TempDir", "", "KAHOY-").Once().Return("/tmp/KAHOY-42", nil)
+				mfs.On("RemoveAll", "/tmp/KAHOY-42").Once().Return(nil)
+
+				expK8sResources1 := []model.K8sObject{newK8sObject("test1", "ns1")}
+				mk.On("EncodeObjects", mock.Anything, expK8sResources1).Once().Return([]byte("test1"), nil)
+				mfs.On("WriteFile", "/tmp/KAHOY-42/apps.v1.Deployment.ns1.test1", []byte("test1"), mock.Anything).Once().Return(nil)
+				expCmd1 := expCmdMatcher(
+					[]string{"diff", "-u", "-N", "/tmp/KAHOY-42/apps.v1.Deployment.ns1.test1", "-"},
+					"",
+				)
+				mc.On("Run", mock.MatchedBy(expCmd1)).Once().Return(nil)
+
+				expK8sResources2 := []model.K8sObject{newK8sObject("test2", "ns2")}
+				mk.On("EncodeObjects", mock.Anything, expK8sResources2).Once().Return([]byte("test2"), nil)
+				mfs.On("WriteFile", "/tmp/KAHOY-42/apps.v1.Deployment.ns2.test2", []byte("test2"), mock.Anything).Once().Return(nil)
+				expCmd2 := expCmdMatcher(
+					[]string{"diff", "-u", "-N", "/tmp/KAHOY-42/apps.v1.Deployment.ns2.test2", "-"},
+					"",
+				)
+				mc.On("Run", mock.MatchedBy(expCmd2)).Once().Return(nil)
+
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert := assert.New(t)
+			require := require.New(t)
+
+			// Mocks.
+			menc := &kubectlmock.K8sObjectEncoder{}
+			mcmd := &kubectlmock.CmdRunner{}
+			mfs := &kubectlmock.FSManager{}
+			test.mock(menc, mcmd, mfs)
+
+			// Prepare.
+			test.config.Out = ioutil.Discard
+			test.config.YAMLEncoder = menc
+			test.config.CmdRunner = mcmd
+			test.config.FSManager = mfs
+			manager, err := kubectl.NewDiffManager(test.config)
+			require.NoError(err)
+
+			// Execute.
+			err = manager.Delete(context.TODO(), test.resources)
+
+			// Check.
+			if test.expErr {
+				assert.Error(err)
+			} else if assert.NoError(err) {
+				menc.AssertExpectations(t)
+				mcmd.AssertExpectations(t)
+				mfs.AssertExpectations(t)
+			}
+		})
+	}
+}
